8599ce67e723764a95c1832e0f453e3560d7bb3e
abdellah <als.abdellah@gmail.com>
2022-09-22 10:51:05 +0300

    working state - CSS

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import { createExtension } from "base-extension";
+import { createExtension } from "../stylesheet-extensions/packages/base-extension";
 
 import Generator from "./generator";
 import { COPYRIGHT } from "./constants";
 
-const exportPrefix = `${COPYRIGHT}\n\n`;
+const exportPrefix = "${COPYRIGHT}\n\n";
 
-const INDENTATION =  "  ";
+const INDENTATION = "  ";
 
+// @ts-ignore
 export default createExtension({
-    language: "css",
-    Generator,
-    colorsOptions: {
-        prefix: `:root {\n${INDENTATION}`,
-        separator: `\n${INDENTATION}`,
-        suffix: "\n}"
-    },
-    spacingOptions: {
-        prefix: `:root {\n${INDENTATION}`,
-        separator: `\n${INDENTATION}`,
-        suffix: "\n}"
-    },
-    exportTextStylesOptions: {
-        prefix: exportPrefix
-    },
-    exportColorsOptions: {
-        prefix: exportPrefix
-    },
-    exportSpacingOptions: {
-        prefix: exportPrefix
-    }
+	language: "css",
+	Generator,
+	colorsOptions: {
+		prefix: ":root {\n${INDENTATION}",
+		separator: "\n${INDENTATION}",
+		suffix: "\n}"
+	},
+	spacingOptions: {
+		prefix: ":root {\n${INDENTATION}",
+		separator: "\n${INDENTATION}",
+		suffix: "\n}"
+	},
+	exportTextStylesOptions: {
+		prefix: exportPrefix
+	},
+	exportColorsOptions: {
+		prefix: exportPrefix
+	},
+	exportSpacingOptions: {
+		prefix: exportPrefix
+	}
 });
