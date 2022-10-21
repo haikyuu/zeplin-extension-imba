6d967106e44ac7a5e30d2fdb064a3e3b049e7737
abdellah <als.abdellah@gmail.com>
2022-10-21 11:15:27 +0300

    remove console.logs

--- src/map-style.js
+++ src/map-style.js
@@ -13,7 +13,6 @@ export default function mapΞstyle(declarations,cls,cssΞvars,mod){
 	for (let $1 = 0, $2 = iter$__(declarations), $12 = $2.length; $1 < $12; $1++) {
 		let declaration = $2[$1];
 		if (declaration.prop.startsWith('--')) { continue; };
-		console.log("dec",declaration);
 		let {prop: _prop,value: value} = declaration;
 		if (declaration.prop === 'color' || declaration.value.includes('rgb(')) {
 			
@@ -122,7 +121,6 @@ export default function mapΞstyle(declarations,cls,cssΞvars,mod){
 					if (decl.__global) { continue; };
 					let withΞfallback = m[1].matchAll(/([^,]*),([^,]*)/g);
 					withΞfallback = [...withΞfallback];
-					console.log("ss",style.value);
 					if (withΞfallback.length) {
 						
 						if (style.value.includes(m[0])) {
